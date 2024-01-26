ddb8eaa57a772d34080b688c750201264cdda5e6
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2024-01-26 10:06:02 +0100

    added start script in package.json

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use((_, res) => {
   res.status(404).json({ success: false, error: "Route not found" });
 });
 
-const PORT = 3064;
-const HOST = "127.0.0.1";
+const PORT = process.env.PORT || 3064;
+// const HOST = "127.0.0.1";
 
-app.listen(PORT, HOST, () => console.log(`Server runs on ${HOST}:${PORT}`));
+app.listen(PORT, () => console.log(`Server runs on ${PORT}`));
